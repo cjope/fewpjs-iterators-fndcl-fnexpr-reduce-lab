0dc9856bc09b3429c41feb176e00ebe3332adb4d
Craig <craigjope@gmail.com>
2021-06-25 09:01:44 -0600

    commit tree 91cc168631bbb84eadc7e09f536c4e9ff13f5565

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, num) => accumulator + num)
+
+//Not sure what this would be used for... seems more practical as a function:
+// const totalBatteries = () => batteryBatches.reduce((accumulator,num) => accumulator + num)
